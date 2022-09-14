0c4a06cddc38d4f88a1b7ecf9edea256844d7b78
DongwooKim <kimphysicsman@gmail.com>
2022-09-14 15:51:32 +0900

    feat/ 상픔 앱 생성 & 모델링

---
+++ product/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Product(models.Model):
+    STATUS_CHOICES = (
+        ("ready", "판매준비중"),
+        ("sale", "판매중"),
+        ("soldout", "판매완료"),
+    )
+
+    name = models.CharField("이름", max_length=20)
+    description = models.CharField("설명", max_length=200, default="")
+    address = models.CharField("판매지", max_length=100, default="")
+    delivery_cost = models.IntegerField("배송비", default=0)
+    status = models.CharField("판매상태", max_length=20, choices=STATUS_CHOICES, default="ready")
+
+    def __str__(self):
+        return self.name
+
+
+class Price(models.Model):
+    product = models.ForeignKey(Product, verbose_name="상품", on_delete=models.CASCADE)
+    unit = models.CharField("판매단위", max_length=100)
+    price = models.IntegerField("가격")
+    stock = models.IntegerField("재고", default=0)
+
+    def __str__(self):
+        return f"{self.product.name} : {self.unit} - {str(self.price)}"
